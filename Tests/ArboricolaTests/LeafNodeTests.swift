5834efd6ed734eb6c3dc8f437aa11296426f0347
Christopher Richez <crichez@pm.me>
2022-07-12 16:13:42 -0400

    Added test comments and docs.

--- Tests/ArboricolaTests/LeafNodeTests.swift
+++ Tests/ArboricolaTests/LeafNodeTests.swift
@@ -23,20 +23,26 @@ import XCTest
 
 class LeafNodeTests: XCTestCase {
     /// Asserts the iterator returns all expected leaves.
-    /// 
-    /// This test manually assembles a list of leaves to avoid using the insert method.
     func testIterator() {
+        // Initialize each leaf separately.
         let first = Leaf(key: 0, value: 0.0)
         let next = Leaf(key: 1, value: 1.0)
         let last = Leaf(key: 2, value: 2.0)
 
+        // Chain the leaves together.
         first.next = .leaf(next)
         next.next = .leaf(last)
         last.next = .none
         
+        // Initialize a node that points to the first leaf.
         let node = LeafNode(first: first, count: 3)
 
+        // Move the leaves to an Array.
+        // This uses the LeafNode.Iterator type to populate the Array,
+        // so is equivalent to a for loop.
         let iteratedLeaves = Array(node)
+
+        // Check the contents of each leaf against expectations.
         XCTAssertEqual(iteratedLeaves[0].key, 0)
         XCTAssertEqual(iteratedLeaves[0].value, 0.0)
         XCTAssertEqual(iteratedLeaves[1].key, 1)
@@ -45,20 +51,23 @@ class LeafNodeTests: XCTestCase {
         XCTAssertEqual(iteratedLeaves[2].value, 2.0)
     }
 
-    /// Asserts the insert method inserts leaves as expected.
-    /// 
-    /// This test depends on `testIterator`.
-    func testInsertOne() {
+    /// Asserts inserting a leaf at the end of a leaf node succeeds as expected.
+    func testInsertOneAtEnd() {
+        // Initialize a node with a single leaf.
         let firstLeaf = Leaf(key: 0, value: 0.0)
         let node = LeafNode(first: firstLeaf, count: 1)
 
+        // Insert a new key-value pair that should fit after the first leaf.
         let (inserted, exceeded) = node.insert(key: 1, value: 1.0)
         XCTAssertTrue(inserted)
         XCTAssertFalse(exceeded)
 
         switch node.first.next {
         case .leaf(let insertedLeaf):
+            // Assert the leaf was inserted at the right position with the right key and value.
             XCTAssertEqual(insertedLeaf.key, 1)
+            XCTAssertEqual(insertedLeaf.value, 1.0)
+            XCTAssertNil(insertedLeaf.next)
         default:
             XCTFail("Unexpected end of node.")
         }
